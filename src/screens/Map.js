b4c08daaf6f9da6266fa91fbf22656d014e71d70
nahuelhsouto <nahuelhsouto@gmail.com>
2021-10-28 20:49:46 -0300

    Inicio

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const Map = () => {
@@ -10,9 +10,12 @@ const Map = () => {
     longitudeDelta: 0.0421,
   };
   return (
-    <View style={{flex:1,backgroundColor:'red'}}>
-      <MapView style={{flex:1}}
-      provider={PROVIDER_GOOGLE} initialRegion={initialRegion} />
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <MapView
+        style={{flex: 1}}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={initialRegion}
+      />
     </View>
   );
 };
